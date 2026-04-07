b455f1a2701194fbf41e87f372d469ae356e6deb
Otto Metaxas <ometaxas@gmail.com>
2026-04-07 14:25:51 +0100

    fix: add settings hydration to RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,36 +6,39 @@ import { useAlertStore } from '../store/useAlertStore';
 import { usePriceFetcher } from '../hooks/usePriceFetcher';
 import { useAlertChecker } from '../hooks/useAlertChecker';
 import { requestNotificationPermission } from '../services/notifications';
+import { useSettingsStore } from '../store/useSettingsStore';
 
 function AppInitializer() {
-  usePriceFetcher();
-  useAlertChecker();
-  return null;
+    usePriceFetcher();
+    useAlertChecker();
+    return null;
 }
 
 export default function RootLayout() {
-  const hydrateWatchlist = useWatchlistStore(state => state.hydrate);
-  const hydratePrices = usePriceStore(state => state.hydrate);
-  const hydrateAlerts = useAlertStore(state => state.hydrate);
+    const hydrateWatchlist = useWatchlistStore((state) => state.hydrate);
+    const hydratePrices = usePriceStore((state) => state.hydrate);
+    const hydrateAlerts = useAlertStore((state) => state.hydrate);
+    const hydrateSettings = useSettingsStore((state) => state.hydrate);
 
-  useEffect(() => {
-    hydrateWatchlist();
-    hydratePrices();
-    hydrateAlerts();
-    requestNotificationPermission();
-  }, []);
+    useEffect(() => {
+        hydrateWatchlist();
+        hydratePrices();
+        hydrateAlerts();
+        hydrateSettings();
+        requestNotificationPermission();
+    }, []);
 
-  return (
-    <>
-      <AppInitializer />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          headerStyle: { backgroundColor: '#111827' },
-          headerTintColor: '#fff',
-          contentStyle: { backgroundColor: '#111827' },
-        }}
-      />
-    </>
-  );
+    return (
+        <>
+            <AppInitializer />
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    headerStyle: { backgroundColor: '#111827' },
+                    headerTintColor: '#fff',
+                    contentStyle: { backgroundColor: '#111827' },
+                }}
+            />
+        </>
+    );
 }
